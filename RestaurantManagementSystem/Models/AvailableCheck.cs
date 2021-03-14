47a5df6bd7d014e0ddc4e1e0dac9c00363fd47b8
lalit <lalit@adequateinfosoft.com>
2021-03-15 03:44:28 +0530

    push first

---
+++ RestaurantManagementSystem/Models/AvailableCheck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantManagementSystem.Models
+{
+    public class AvailableCheck
+    {
+        public string StartDate { get; set; }
+        public string selectedtime { get; set; }
+        public string selectedslot { get; set; }
+    }
+}
